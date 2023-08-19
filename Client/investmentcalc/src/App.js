d6017fe4f54221fb9e71212bdb58884b92f237d8
sudhaScode <sunriser7995@gmail.com>
2023-08-19 18:39:34 +0530

    FULL

--- Client/investmentcalc/src/App.js
+++ Client/investmentcalc/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import logo from "./assets/logo.png";
+import React,{useState} from 'react';
 import './App.css';
+import FinanceForm from "./FinanceForm";
+import FinanceTable from "./FinanceTable";
 
 function App() {
-  return (
+  const [finance, setFinance] = useState(null);
+  const [useDecision, setUserdecision] =useState(true);
+  const financeCalculator = (userInput)=>{
+      setFinance(userInput)
+  }
+  const yearlyData = []; // per-year results
+  if (finance != null){
+    let currentSavings = finance["currentSavings"]; // feel free to change the shape of this input object!
+    const yearlyContribution  = finance["yearlySavings"]; // as mentioned: feel free to change the shape...
+    const expectedReturn = finance["expectedIntrest"];
+    const duration = finance["investmentDuration"];
+    let investmentCapital = currentSavings;
+    let totalIntrest =0;  
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      investmentCapital +=yearlyContribution;//clear
+      const yearlyInterest = (currentSavings  * expectedReturn)/100; 
+      totalIntrest += yearlyInterest;
+      currentSavings = investmentCapital + totalIntrest;   
+      yearlyData.push({ 
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        totalIntrest: totalIntrest,
+        investmentCapital: investmentCapital,
+      }); 
+    }
+  }
+  const clearHandler =(decision)=>{
+    setUserdecision(decision);      
+  };
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className= "header">
+        <img src = {logo} alt = "logo" className = "header-img"/>
+        <h1>Investment Calculator</h1>
       </header>
+       <FinanceForm onCalculater={financeCalculator} onClearHandler = {clearHandler}/>
+       {finance && useDecision ?<FinanceTable data={yearlyData}/>: <p>Set the financial goals</p>}
     </div>
   );
 }
-
 export default App;
